cfe154fa32aa1265ba9c13b573cea0a4e9a8bfb8
xjhjoe <xjh.joe@gmail.com>
2026-09-08 09:52:06 +0800

    Bump version for Shizuku reconnect build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.fivegtile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
