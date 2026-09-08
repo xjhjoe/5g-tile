c296b51e6f7f6c0e49dc104c9e13b63c2c807817
xjhjoe <xjh.joe@gmail.com>
2026-09-08 10:33:50 +0800

    Bump version to 2.1 authorization fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.fivegtile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.0-final"
+        versionCode = 5
+        versionName = "2.1-final"
     }
 
     buildFeatures {
