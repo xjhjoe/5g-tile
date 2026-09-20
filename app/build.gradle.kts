a8d792171a1b9281f8dfb29d7686c391db21beba
xjhjoe <xjh.joe@gmail.com>
2026-09-20 18:26:05 +0800

    Bump Android 17 fix build to 2.2-preview2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.fivegtile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "2.2-preview"
+        versionCode = 7
+        versionName = "2.2-preview2"
     }
 
     buildFeatures {
