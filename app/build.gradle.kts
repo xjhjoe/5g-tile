f2f2fb0e2714987b5661d285f3cf343b4a1af75f
xjhjoe <xjh.joe@gmail.com>
2026-09-20 18:33:45 +0800

    Bump app-button build to 2.2-preview3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.fivegtile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "2.2-preview2"
+        versionCode = 8
+        versionName = "2.2-preview3"
     }
 
     buildFeatures {
