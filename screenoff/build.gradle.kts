3ad1f149dcf944fc7bb1ba2be3913b7aa91591ff
xjhjoe <xjh.joe@gmail.com>
2026-09-23 19:41:22 +0800

    Keep upstream source namespace with isolated app id

--- screenoff/build.gradle.kts
+++ screenoff/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
 }
 android {
-    namespace = "com.tile.screenoff.a17"
+    namespace = "com.tile.screenoff"
     compileSdk = 35
     defaultConfig {
         applicationId = "com.tile.screenoff.a17"
