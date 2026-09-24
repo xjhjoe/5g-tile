bac81122c7cb1a3aaceb0b9fd055c35a6fb03a42
xjhjoe <xjh.joe@gmail.com>
2026-09-24 17:25:10 +0800

    Bump ScreenOff A17 preview3

--- screenoff/build.gradle.kts
+++ screenoff/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.tile.screenoff.a17"
         minSdk = 21
         targetSdk = 35
-        versionCode = 23
-        versionName = "23-a17-preview2"
+        versionCode = 24
+        versionName = "24-a17-preview3"
     }
     buildFeatures { aidl = true }
     buildTypes { release { isMinifyEnabled = false } }
